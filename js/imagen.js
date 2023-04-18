0b64a8b93dd06a7f25736e0813cb620e56c8043d
Jose Antonio Ponce Mtz <131209650+antoniomartinez22@users.noreply.github.com>
2023-04-18 15:58:46 -0600

    Add files via upload

---
+++ js/imagen.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //aparecer imagen
+    setTimeout(function(){
+        $(".content").fadeOut(1200);
+    },1800);
+// desaperecer imagen 
+    setTimeout(function(){
+        $(".content2").fadeIn(1200);
+    },2500);
+});
